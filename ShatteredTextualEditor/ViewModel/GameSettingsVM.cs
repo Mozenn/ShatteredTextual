8b9b02b60d9fb5ebe0dca0b99582cdd141b8a0f1
Cassany Gauthier <gauthier.cassany@gmail.com>
2019-09-04 13:04:34 +0200

    UI Layout ongoing Begin of MVVM setup

---
+++ ShatteredTextualEditor/ViewModel/GameSettingsVM.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShatteredTextualEditor.ViewModel
+{
+    public class GameSettingsVM : INotifyPropertyChanged
+    {
+
+        // TODO : add delete, add & edit features 
+
+        public GameSettingsVM()
+        {
+            ProgressionEvents = new ObservableCollection<string>();
+
+            // TODO : Initialize ProgressionEvents & SelectedProgEvent 
+        }
+
+        // ------------- INOTIFYPROPERTYCHANGED INTERFACE 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // -------------- PROGRESSION EVENTS 
+        public ObservableCollection<string> ProgressionEvents { get; set; }
+
+        private string selectedProgressionEvent;
+
+        public event EventHandler SelecteProgressionEventChanged;
+
+        public string SelecteProgressionEvent
+        {
+            get { return selectedProgressionEvent; }
+            set
+            {
+                selectedProgressionEvent = value;
+                SelecteProgressionEventChanged(this, new EventArgs());
+                OnPropertyChanged("SelecteProgressionEvent");
+            }
+        }
+
+    }
+}
